ccd5445c25c315ffb6e4bc9d3993c20b7685013e
yanpengfei <313508737@qq.com>
2021-12-29 22:21:05 +0800

    dev分支上提交

--- helloGit/src/main/java/com/fei/pojo/User.java
+++ helloGit/src/main/java/com/fei/pojo/User.java
@@ -4,6 +4,8 @@ public class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+
+
     }
 
     public User() {
@@ -11,6 +13,7 @@ public class User {
 
     private String username;
     private String password;
+    private int age;
 
     public String getUsername() {
         return username;
