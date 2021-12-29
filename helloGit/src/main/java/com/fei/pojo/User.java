5eefeba6a08f17195f72cdb7a2c58052f72efefb
yanpengfei <313508737@qq.com>
2021-12-29 22:26:41 +0800

    提交

--- helloGit/src/main/java/com/fei/pojo/User.java
+++ helloGit/src/main/java/com/fei/pojo/User.java
@@ -1,41 +1,12 @@
 package com.fei.pojo;
 
 public class User {
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-
-
-    }
-
-    public User() {
-    }
 
     private String username;
     private String password;
     private int age;
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
