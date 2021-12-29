e0862a2173acb4cb0086feaa39695a2af748bacd
yanpengfei <313508737@qq.com>
2021-12-29 22:08:07 +0800

    第二次提交

--- helloGit/src/main/java/com/fei/pojo/User.java
+++ helloGit/src/main/java/com/fei/pojo/User.java
@@ -1,6 +1,13 @@
 package com.fei.pojo;
 
 public class User {
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
 
     private String username;
     private String password;
